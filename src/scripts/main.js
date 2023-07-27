28d256fb886ab51b36da78eac0e78984f1067762
Giovanni Perotto <giovanniperotto@hotmail.com>
2023-07-26 22:00:15 -0300

    feat: add game over conditions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
 const canvas = document.getElementById('canvas')
 const c = canvas.getContext('2d')
+const span = document.getElementsByTagName('h2')[0]
 
 canvas.width = 400
 canvas.heigth = 400
+
+const colision = () => {
+  for (let i = 0; i < snakeBody.length; i++) {
+    let part = snakeBody[i]
+    if (part.x == player_snake.x && part.y == player_snake.y) {
+      return true
+    }
+  }
+  return false
+}
 
 const drawMap = () => {
   c.fillStyle = 'black'
@@ -10,21 +21,60 @@ const drawMap = () => {
 }
 
 const animate = () => {
+  player_snake.update()
+  if (gameOver()) return
   drawMap()
   food.draw()
-  player_snake.update()
   player_snake.eatFood()
   player_snake.draw()
+  //drawScore()
   setTimeout(animate, 1000 / speed)
 }
 
-const horizontalBlocks = 40
-const verticalBlocks = 40
-const blockSize = canvas.width / horizontalBlocks - 2
+const horizontalBlocks = 20
+const verticalBlocks = 20
+const blockSize = canvas.width / horizontalBlocks
 
-let speed = 10
+const initialSpeed = 5
+let speed = initialSpeed
 let horizontalVelocity = 0
 let verticalVelocity = 0
+
+let score = 0
+
+const gameOver = () => {
+  if (horizontalVelocity == 0 && verticalVelocity == 0) {
+    return false
+  }
+  if (
+    player_snake.x < 0 ||
+    player_snake.x >= canvas.width ||
+    player_snake.y < 0 ||
+    player_snake.y >= canvas.heigth
+  ) {
+    drawGameOver()
+    return true
+  }
+
+  if (colision()) {
+    drawGameOver()
+    return true
+  }
+  return false
+}
+//colocar a info em um <span>
+const drawScore = () => {
+  c.fillStyle = 'white'
+  c.font = '14px serif'
+  c.fillText('Score:' + score, canvas.width - 75, 15)
+  span.innerText = `Score: ${score}`
+}
+
+const drawGameOver = () => {
+  c.fillStyle = 'white'
+  c.font = '40px serif'
+  c.fillText('Game Over', canvas.width / 4, canvas.heigth / 2)
+}
 // ========== Control ==========
 const keyDown = event => {
   switch (event.keyCode) {
@@ -54,16 +104,29 @@ const keyDown = event => {
 document.body.addEventListener('keydown', keyDown)
 
 // ========== Snake ==========
-
+const snakeBody = []
+let tailLength = 20
 class Snake {
   constructor(x, y, w, h) {
     this.x = x
     this.y = y
     this.w = w
     this.h = h
+
     this.draw = () => {
+      for (let i = 0; i < snakeBody.length; i++) {
+        c.fillStyle = `rgb(${(snakeBody.length - i) * 10},255,0)`
+        const part = snakeBody[i]
+        c.fillRect(part.x, part.y, w, h)
+      }
+
+      snakeBody.push(new SnakePart(this.x, this.y))
+
+      if (snakeBody.length > tailLength) {
+        snakeBody.shift()
+      }
       c.fillStyle = 'green'
-      c.fillRect(this.x, this.y, blockSize, blockSize)
+      c.fillRect(this.x, this.y, w, h)
     }
 
     this.update = () => {
@@ -74,16 +137,28 @@ class Snake {
     this.eatFood = () => {
       if (this.x == food.x && this.y == food.y) {
         food.respaw()
+        tailLength++
+        score++
+        if (speed < 20) {
+          speed = Math.floor(tailLength / 2) + initialSpeed
+        }
       }
     }
+  }
+}
+
+class SnakePart {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
   }
 }
 
 const player_snake = new Snake(
   (canvas.width / blockSize / 2) * blockSize,
   (canvas.heigth / blockSize / 2) * blockSize,
-  blockSize,
-  blockSize
+  blockSize - 2,
+  blockSize - 2
 )
 // ========== Food ==========
 class Food {
@@ -94,7 +169,7 @@ class Food {
     this.h = h
     this.draw = () => {
       c.fillStyle = 'red'
-      c.fillRect(this.x, this.y, blockSize, blockSize)
+      c.fillRect(this.x, this.y, w, h)
     }
     this.respaw = () => {
       this.x =
@@ -108,8 +183,8 @@ class Food {
 const food = new Food(
   Math.floor(Math.random() * (canvas.width / blockSize)) * blockSize,
   Math.floor(Math.random() * (canvas.heigth / blockSize)) * blockSize,
-  blockSize,
-  blockSize
+  blockSize - 2,
+  blockSize - 2
 )
 // ========== Initialize ==========
 animate()
